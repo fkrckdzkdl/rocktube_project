843664ed7da4b66b07a4bbe6119fa87978675f42
fkrckdzkdl <91944913+fkrckdzkdl@users.noreply.github.com>
2022-12-02 16:19:50 +0900

    axios - api  활용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import SearchHeader from './components/SearchHeader';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      {/* provider로 감싼 컴포넌트에서 쿼리클라이언트 사용이 가능하다 */}
+      <QueryClientProvider client={queryClient}>  
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { json, useParams } from 'react-router-dom';
+import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>;
+  const {isLoading, error, data:videos} = useQuery(
+    ['videos',keyword],
+    // 진짜 api 사용
+    // async ()=>{return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=오킹&key=AIzaSyBGbyR4X5y1I_zZxS6VPGF8ouWhVT6HTEg`)
+    // mock data 사용
+    // fatch 사용
+    // async ()=>{return fetch(`/videos/${keyword? 'search':'popular'}.json`)
+    // .then((res)=>res.json())
+    // .then((data)=>data.items);
+    // axios 사용
+    async ()=>{
+      return axios.get(`/videos/${keyword? 'search':'popular'}.json`)
+      .then((res)=>res.data.items);
+  });
+  return(
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>;
+      {isLoading&& <p>Loading...</p>}
+      {error && <p>SomeThing is worng</p>}
+      {videos && <ul>
+        {videos.map(video => <VideoCard key={video.id} video={video}/>)}
+        </ul>}
+    </>
+    )
 }
